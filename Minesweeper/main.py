5634a8eb0682da9ef76ca55256b73564d7654ba7
João Simão <jsimao@illumina.com>
2020-01-16 14:04:58 +0000

    Finished Minesweeper

--- Minesweeper/main.py
+++ Minesweeper/main.py
@@ -5,16 +5,20 @@ from Minesweeper.atomicfile import InputAtomicFile, OutputAtomicFile
 
 game_file = "../../codekata-minesweeper/game.json"
 command_file = "../../codekata-minesweeper/command.json"
+mines = set()
 
 def main():
     status = "playing"
     board = read_mine_file()
     print_board(board)
     while status == "playing":
+        find_mines(board)
         moves = get_moves(board)
         write_move_file(moves)
         board = read_mine_file()
         print_board(board)
+        if (len(mines) == board["nmines"]):
+            status = "Finished"
 
 def read_mine_file():
     with InputAtomicFile(game_file) as handle:
@@ -30,15 +34,19 @@ def print_board(board):
     # "grid": [ [cell, ...], [cell, ...], ...],
     # "nmines": integer
     # }
-    for row in board["grid"]:
-        for cell in row:
-            print(cell, end="")
+    for r in range(0, board["nrows"]):
+        for c in range (0, board["ncols"]):
+            if (r, c) in mines:
+                print("#", end = "")
+            else:
+                print(board["grid"][r][c], end="")
         print()
     print()
 
+
 def get_moves(board):
     moves = []
-    moves.extend(find_zeroes(board))
+    moves.extend(find_blanks(board))
 
     if moves == []:
         row = random.randint(0, board["nrows"])
@@ -47,23 +55,49 @@ def get_moves(board):
 
     return moves
 
-def find_zeroes(board):
+
+def find_mines(board):
+    for r in range(0, board["nrows"]):
+        for c in range (0, board["ncols"]):
+            if board["grid"][r][c] != "?" and board["grid"][r][c] != "#":
+                nbors = get_nbors(r, c, board)
+                unknowns = get_unknown_nbors(nbors, board)
+                if len(unknowns) > 0:
+                    nbor_mines = set(get_mines_in_nbors(nbors))
+                    nbor_mines.update(unknowns)
+                    if len(nbor_mines) == board["grid"][r][c]:
+                        mines.update(nbor_mines)
+                        for (r, c) in mines:
+                            board["grid"][r][c] = "#"
+
+
+def find_blanks(board):
     moveset = set()
     for r in range(0, board["nrows"]):
         for c in range (0, board["ncols"]):
-            if board["grid"][r][c] == 0:
-                add_nbors(moveset, r, c, board)
+            if board["grid"][r][c] != "?" and board["grid"][r][c] != "#":
+                nbors = get_nbors(r, c, board)
+                mines = get_mines_in_nbors(nbors)
+                if board["grid"][r][c] == len(mines):
+                    moveset.update(get_unknown_nbors(nbors, board))
 
     return list(map(lambda cell: position(cell[0], cell[1]), moveset))
 
-def add_nbors(moveset, r, c, board):
-    for i in range(r - 1, r + 2):
-        for j in range(c - 1, c + 2):
-            if i >= 0 and i < board["ncols"] and j >= 0 and j < board["nrows"] and not (i == r and j == c) and board["grid"][i][j] == "?":
-                moveset.add((i, j))
+
+def get_nbors(r, c, board):
+    return [(i, j) for i in range(r - 1, r + 2) for j in range(c - 1, c + 2) if i >= 0 and i < board["nrows"] and j >= 0 and j < board["ncols"] and not (i == r and j == c)]
+
+
+def get_mines_in_nbors(nbors):
+    return [(i, j) for (i, j) in nbors if (i, j) in mines]
+
+
+def get_unknown_nbors(nbors, board):
+    return [(i, j) for (i, j) in nbors if board["grid"][i][j] == "?"]
 
 def position(r, c):
     return {"x": c, "y": r}
+
 
 def write_move_file(moves):
     #{
